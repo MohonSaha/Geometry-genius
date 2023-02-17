31050818f9c732b54ce25724a6852c4bb69a820d
MohonSaha <practicewithmohon@gmail.com>
2023-02-18 00:35:25 +0600

    design complite

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('triangle-btn').addEventListener('click', function(){
+    const triangleSide = document.getElementById('triangle-side').value;
+    console.log(triangleSide);
+
+
+
+    document.getElementById('triangle-side').value = '';
+})
